795452ce4cf894298f11fedd8ce928bf3984aba5
guero169 <gp1714@ic.ac.uk>
2017-05-04 00:55:33 +0100

    Chapter 3 + 4
    speech to text and text to speech

--- Chapter03/index.js
+++ Chapter03/index.js
@@ -35,9 +35,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('appName', 'z2c-chapter03');
 // disable the following line in Bluemix. App will start on port 6003 in Bluemix
-app.set('port', process.env.PORT || 6003);
+// app.set('port', process.env.PORT || 6003);
 // enable the following line in Bluemix
-// app.set('port', appEnv.port);
+app.set('port', appEnv.port);
 
 app.set('views', path.join(__dirname + '/HTML'));
 app.engine('html', require('ejs').renderFile);
