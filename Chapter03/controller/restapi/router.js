fd60decd9dcb95c14b24b887b68352fdb5e84b5e
Bob Dill <rddill@us.ibm.com>
2018-11-12 14:24:59 -0500

    correct naming error in router.js C3

--- Chapter03/controller/restapi/router.js
+++ Chapter03/controller/restapi/router.js
@@ -4,4 +4,4 @@ var speech_to_text = require('./features/speech_to_text');
 
 module.exports = router;
 // speech-to-text
-router.get('/api/speech-to-text/token*',speech_to_text.token);
+router.get('/api/speech-to-text/token*',speech_to_text.stt_token);
